8d24432787d9b4e8a0b9c10d8190b6a1a61836b5
puddingoverdose <ridho050302@gmail.com>
2022-11-10 14:24:29 +0700

    Update form

--- pudding/lib/form.dart
+++ pudding/lib/form.dart
@@ -15,7 +15,11 @@ class _MyFormPageState extends State<MyFormPage> {
   bool jenjangDiploma = false;
   bool jenjangMagister = false;
   bool jenjangDoktor = false;
-  
+  double umur = 0;
+  String kelasPBP = 'A';
+  List<String> listKelasPBP = ['A', 'B', 'C', 'D', 'E', 'F', 'KI'];
+  bool _nilaiSwitch = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,52 +53,205 @@ class _MyFormPageState extends State<MyFormPage> {
           ],
         ),
       ),
-       body: Form(
-            key: _formKey,
-            child: SingleChildScrollView(
-               child: Container(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(children: [
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Container(
+            margin: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
                 Padding(
-                    // Menggunakan padding sebesar 8 pixels
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                        decoration: InputDecoration(
-                            hintText: "Contoh: Pak Dengklek",
-                            labelText: "Nama Lengkap",
-                            // Menambahkan icon agar lebih intuitif
-                            icon: const Icon(Icons.people),
-                            // Menambahkan circular border agar lebih rapi
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(5.0),
-                            ),
-                        ),
-                        // Menambahkan behavior saat nama diketik 
-                        onChanged: (String? value) {
-                            setState(() {
-                                _namaLengkap = value!;
-                            });
-                        },
-                        // Menambahkan behavior saat data disimpan
-                        onSaved: (String? value) {
-                            setState(() {
-                                _namaLengkap = value!;
-                            });
-                        },
-                        // Validator sebagai validasi form
-                        validator: (String? value) {
-                            if (value == null || value.isEmpty) {
-                                return 'Nama lengkap tidak boleh kosong!';
-                            }
-                            return null;
-                        },
+                  // Menggunakan padding sebesar 8 pixels
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Contoh: Pak Dengklek",
+                      labelText: "Nama Lengkap",
+                      // Menambahkan icon agar lebih intuitif
+                      icon: const Icon(Icons.people),
+                      // Menambahkan circular border agar lebih rapi
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
                     ),
+                    // Menambahkan behavior saat nama diketik
+                    onChanged: (String? value) {
+                      setState(() {
+                        _namaLengkap = value!;
+                      });
+                    },
+                    // Menambahkan behavior saat data disimpan
+                    onSaved: (String? value) {
+                      setState(() {
+                        _namaLengkap = value!;
+                      });
+                    },
+                    // Validator sebagai validasi form
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Nama lengkap tidak boleh kosong!';
+                      }
+                      return null;
+                    },
+                  ),
                 ),
-            ],),
-               ),
+                const ListTile(
+                  leading: Icon(Icons.school),
+                  title: Text("Jenjang"),
+                ),
+                CheckboxListTile(
+                  title: const Text('Sarjana'),
+                  value: jenjangSarjana,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      jenjangSarjana = value!;
+                      if (value) {
+                        jenjangMagister =
+                            jenjangDiploma = jenjangDoktor = false;
+                      }
+                    });
+                  },
+                ),
+                CheckboxListTile(
+                  title: const Text('Diploma'),
+                  value: jenjangDiploma,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      jenjangDiploma = value!;
+                      if (value) {
+                        jenjangMagister =
+                            jenjangSarjana = jenjangDoktor = false;
+                      }
+                    });
+                  },
+                ),
+                CheckboxListTile(
+                  title: const Text('Magister'),
+                  value: jenjangMagister,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      jenjangMagister = value!;
+                      if (value) {
+                        jenjangDiploma = jenjangSarjana = jenjangDoktor = false;
+                      }
+                    });
+                  },
+                ),
+                CheckboxListTile(
+                  title: const Text('Doktor'),
+                  value: jenjangDoktor,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      jenjangDoktor = value!;
+                      if (value) {
+                        jenjangMagister =
+                            jenjangSarjana = jenjangDiploma = false;
+                      }
+                    });
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.co_present),
+                  title: Row(
+                    children: [
+                      Text('Umur: ${umur.round()}'),
+                    ],
+                  ),
+                  subtitle: Slider(
+                    value: umur,
+                    max: 100,
+                    divisions: 100,
+                    label: umur.round().toString(),
+                    onChanged: (double value) {
+                      setState(() {
+                        umur = value;
+                      });
+                    },
+                  ),
+                ),
+                ListTile(
+                  leading: const Icon(Icons.class_),
+                  title: const Text(
+                    'Kelas PBP',
+                  ),
+                  trailing: DropdownButton(
+                    value: kelasPBP,
+                    icon: const Icon(Icons.keyboard_arrow_down),
+                    items: listKelasPBP.map((String items) {
+                      return DropdownMenuItem(
+                        value: items,
+                        child: Text(items),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        kelasPBP = newValue!;
+                      });
+                    },
+                  ),
+                ),
+                SwitchListTile(
+                  title: const Text('Practice Mode'),
+                  value: _nilaiSwitch,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _nilaiSwitch = value;
+                    });
+                  },
+                  secondary: const Icon(Icons.run_circle_outlined),
+                ),
+                TextButton(
+                  child: const Text(
+                    "Simpan",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.blue),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return Dialog(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            elevation: 15,
+                            child: Container(
+                              child: ListView(
+                                padding:
+                                    const EdgeInsets.only(top: 20, bottom: 20),
+                                shrinkWrap: true,
+                                children: <Widget>[
+                                  Center(child: const Text('Informasi Data')),
+                                  SizedBox(height: 20),
+                                  // TODO: Munculkan informasi yang didapat dari form
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text('Kembali'),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    }
+                  },
+                ),
+              ],
             ),
-            ),
-  ),
+          ),
+        ),
+      ),
     );
   }
 }
